c28a7d86aa2ca0b5579f53ed5898960f092b4fbb
Muxutdinov <jmuxutdinov@mail.ru>
2022-04-24 15:21:57 +0500

    add dependencies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import './Navbar.css'
-import portfolio from '../assets/images/portfolio.svg'
-import s from '../assets/icons/s.svg'
+import React from "react";
+import "./Navbar.css";
+import portfolio from "../assets/images/portfolio.svg";
+import s from "../assets/icons/s.svg";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="n-left">
-            <img src={portfolio} alt="" />
-        </div>
-        <div className="n-right">
-                <div className="n-item">About</div>
-                <div className="n-item">Services</div>
-                <div className="n-item">Projects</div>
-                <div className="n-item">Blog</div>
-                <div className="n-item">Book a call</div>
-                <img className='n-item' src={s} alt="" />
-        </div>
+    <div className="navbar">
+      <div className="n-left">
+        <img src={portfolio} alt="" />
+      </div>
+      <div className="n-right">
+        <div className="n-item">About</div>
+        <div className="n-item">Services</div>
+        <div className="n-item">Projects</div>
+        <div className="n-item">Blog</div>
+        <div className="n-item">Book a call</div>
+        <img className="n-item" src={s} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
